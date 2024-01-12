dc50e8a1d8bf9e5a594bc0828978420f6ce66959
tajush <tajusabd@gmail.com>
2024-01-12 07:21:36 +0600

    all

--- src/components/NewsLtrmblBtn.js
+++ src/components/NewsLtrmblBtn.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import icon from "../assets/newsLtricon.svg";
+import icon from "../assets/newsLtrIcon.svg";
 import Image from "next/image";
 const NewsLtrmblBtn = () => {
   return (
     <div className="flex items-center justify-center px-[10px] py-[16px] NewsLtrmblBtn-bg  gap-[5px] h-[52px] w-[181px] md:hidden ">
-      {/* <Image src={icon} alt="icon" /> */}
+      <Image src={icon} alt="icon" />
       <p className="text-[#F8523B]">Schedule a call</p>
     </div>
   );
